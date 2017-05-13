92ef9fbe6969ff6fba7dd846a88f4289f9641718
benBlueKellet <benblue22@gmail.com>
2017-05-13 16:26:26 -0700

    moved game (canvas and keyboard) to its own clas

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Game from './game-model.js';
+import Game from './game.js';
 import jsMouse from './js-mouse.js';
 
 
@@ -12,76 +12,15 @@ class App extends Component {
     this.state = {}
 
     //SETUP Game
-
-    this.state.mouse = {
-      x: 50,
-      y: 50
-    }
-    this.state.winBox = {
-      x: 50,
-      y: 50
-    }
   }
 
   componentDidMount() {
     this.state.can = this.refs.mCanvas
     this.state.ctx = this.state.can.getContext('2d');
-    var mouse = this.state.mouse;
-
-    /**
-     * mouse img is loaded after canvas enviroment setup
-     * @param  {Function} callback is to draw() onload 
-     */
-    ((callback) => {
-      var mouseI = new Image();
-      mouseI.onload = function() {
-        mouse.img = mouseI;
-        callback();
-      }
-      mouseI.src = 'http://www.clker.com/cliparts/A/E/s/N/M/Y/lab-mouse-template-th.png'
-    })(() => {this.draw()});
-
-    window.addEventListener('keydown', (e) => {
-      //console.log(code);
-      var code = e.keyCode;
-      //up arrow
-      if(code === 38) {
-        mouse.y -= 10;
-        this.draw();
-      }
-      //right arriw
-      if(code === 39) {
-        mouse.x += 10;
-        this.draw();
-      }
-      //down arrow
-      if(code === 40) {
-        mouse.y += 10;
-        this.draw();
-      }
-      //left arrow
-      if(code === 37) {
-        mouse.x -= 10;
-        this.draw();
-      }
-    })
-
+    new Game(this.refs.mCanvas);
   }
 
-  checkForWin() {
-    var st = this.state
-    if(st.mouse.x === st.winBox.x && st.mouse.y === st.winBox.y) {
-      console.log("WIN!!!");
-    }
-  }
-
-  draw() {
-    this.checkForWin();
-    this.state.ctx.clearRect(0, 0, this.refs.mCanvas.width, this.refs.mCanvas.height)
-    this.state.ctx.fillRect(this.state.winBox.x, this.state.winBox.y,50,50);
-    this.state.ctx.drawImage(this.state.mouse.img, this.state.mouse.x, this.state.mouse.y, 50, 50)
-  }
-
+  
   render() {
     return (
       <div>
--- src/game.js
+++ src/game.js
@@ -6,8 +6,64 @@
  */
 
 export default class Game {
-	constructor(canvas) {
+	constructor(canvas, controlsScope = window) {
 		this.can = canvas;
 		this.ctx = canvas.getContext('2d');
+		console.log(this);
+		this.ctx.fillRect(90,90,50,50);
+    this.mouse = {}
+    this.mouse.x = 50;
+    this.mouse.y = 50;
+
+    this.winBox = {};
+    this.winBox.x = 50;
+    this.winBox.y = 50;
+		
+		((callback, mouse) => {
+      var mouseI = new Image();
+      mouseI.onload = function() {
+        mouse.img = mouseI;
+        callback();
+      }
+      mouseI.src = 'http://www.clker.com/cliparts/A/E/s/N/M/Y/lab-mouse-template-th.png'
+    })(() => {this.draw()}, this.mouse);
+
+    controlsScope.addEventListener('keydown', (e) => {
+      //console.log(code);
+      var code = e.keyCode;
+      //up arrow
+      if(code === 38) {
+        this.mouse.y -= 10;
+        this.draw();
+      }
+      //right arriw
+      if(code === 39) {
+        this.mouse.x += 10;
+        this.draw();
+      }
+      //down arrow
+      if(code === 40) {
+        this.mouse.y += 10;
+        this.draw();
+      }
+      //left arrow
+      if(code === 37) {
+        this.mouse.x -= 10;
+        this.draw();
+      }
+    })
+	}
+
+	checkForWin() {
+		if(this.mouse.x === this.winBox.x && this.mouse.y === this.winBox.y) {
+			console.log("WIN!!!");
+		}
+	}
+
+	draw() {
+		this.checkForWin();
+		this.ctx.clearRect(0, 0, this.can.width, this.can.height)
+		this.ctx.fillRect(this.winBox.x, this.winBox.y,50,50);
+		this.ctx.drawImage(this.mouse.img, this.mouse.x, this.mouse.y, 50, 50)
 	}
 }
