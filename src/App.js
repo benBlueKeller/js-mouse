5008d6d4ae74418f7cf651de72d154aaab56f689
benBlueKellet <benblue22@gmail.com>
2017-05-13 13:42:22 -0700

    added draw()

--- src/App.js
+++ src/App.js
@@ -16,49 +16,55 @@ class App extends Component {
   }
 
   componentDidMount() {
-    var can = this.refs.mCanvas
-    var ctx = can.getContext('2d');
+    this.state.can = this.refs.mCanvas
+    this.state.ctx = this.state.can.getContext('2d');
     var mouse = this.state.mouse;
     
     function placeMouse(ctx) {
       ctx.drawImage(mouse.img, mouse.x, mouse.y, 50, 50)
     }
 
-    ((callback) => {
+    ((callback, ctx) => {
       var mouseI = new Image();
       mouseI.onload = function() {
         mouse.img = mouseI;
         callback(ctx);
       }
       mouseI.src = 'http://www.clker.com/cliparts/A/E/s/N/M/Y/lab-mouse-template-th.png'
-    })(placeMouse);
+    })(placeMouse, this.state.ctx);
+
     window.addEventListener('keydown', (e) => {
       var code = e.keyCode;
       console.log(code);
       //up arrow
       if(code === 38) {
         mouse.y -= 10;
-        placeMouse(ctx);
+        placeMouse(this.state.ctx);
       }
       //right arriw
       if(code === 39) {
         mouse.x += 10;
-        placeMouse(ctx);
+        placeMouse(this.state.ctx);
       }
       //down arrow
       if(code === 40) {
         mouse.y += 10;
-        placeMouse(ctx);
+        placeMouse(this.state.ctx);
       }
       //left arrow
       if(code === 37) {
         mouse.x -= 10;
-        placeMouse(ctx);
+        placeMouse(this.state.ctx);
       }
     })
 
 
-    ctx.fillRect(50,50,50,50);
+    this.state.ctx.fillRect(50,50,50,50);
+  }
+
+  draw() {
+    this.state.ctx.clearRect(0, 0, this.refs.mCanvas.width, this.refs.mCanvas.height)
+    this.state.ctx.drawImage(this.state.mouse.img, this.state.mouse.x, this.state.mouse.y, 50, 50)
   }
 
   render() {
