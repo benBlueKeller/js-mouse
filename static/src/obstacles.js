3a2bd6c8ff44f6154a42678b6020b735faeaecd5
benBlueKeller <benblue22@gmail.com>
2018-01-09 15:54:09 -0800

    created comments for how to design the FSM obstacle class

--- static/src/obstacles.js
+++ static/src/obstacles.js
@@ -10,7 +10,18 @@ export default class Obstacle {
 
 export class FSM extends Obstacle {
   constructor(obs) {
+    // The FSM is an obstacle that moves in a series of Fibonacci spirals
     super(obs);
+    this.nFibonacci = 0;
+    this.onTick = this.onTick.bind(this);
+  }
 
+  onTick() {
+    //every tick the a new square is laid along the current spiral
+      //if no parameter is defined, a random one is drawn and the squareWidth is zero
+      // FSMs position is moved to the fartherest corner of square
+    this.squareWidth = Math.round((Math.pow(1.618034, this.nFibonacci) - Math.pow(-0.618034, this.nFibonacci)) / Math.sqrt(5));
+    this.current
+    this.nFibonacci++;
   }
 }
