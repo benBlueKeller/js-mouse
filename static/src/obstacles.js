378a54a0dc02b29fb9fe462c8a8004b3746270fc
benBlueKeller <benblue22@gmail.com>
2018-01-11 15:16:29 -0800

    changed approach to FSM movement

--- static/src/obstacles.js
+++ static/src/obstacles.js
@@ -10,9 +10,12 @@ export default class Obstacle {
 
 export class FSM extends Obstacle {
   constructor(obs) {
-    // The FSM is an obstacle that moves in a series of Fibonacci spirals
+    // The FSM is an obstacle that moves in a series of spirals
     super(obs);
-    this.nFibonacci = 0;
+    this.radius = 10;
+    this.angle = 0;
+    this.originX = this.x - this.radius;
+    this.originY = this.y;
     this.onTick = this.onTick.bind(this);
   }
 
@@ -20,8 +23,13 @@ export class FSM extends Obstacle {
     //every tick the a new square is laid along the current spiral
       //if no parameter is defined, a random one is drawn and the squareWidth is zero
       // FSMs position is moved to the fartherest corner of square
-    this.squareWidth = Math.round((Math.pow(1.618034, this.nFibonacci) - Math.pow(-0.618034, this.nFibonacci)) / Math.sqrt(5));
-    this.current
-    this.nFibonacci++;
+    // this.squareWidth = Math.round((Math.pow(1.618034, this.nFibonacci) - Math.pow(-0.618034, this.nFibonacci)) / Math.sqrt(5));
+    this.x = this.originX + Math.cos(this.angle)*this.radius;
+    this.y = this.originY + Math.sin(this.angle)*this.radius;
+    this.angle += 10;
+  }
+
+  draw(ctx) {
+    ctx.fillRect(this.x, this.y, this.w, this.h);
   }
 }
